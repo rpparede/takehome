acbe56f35fbfba06aed68ea40b478f4cf42f01e7
rpparede <rpparede@uwaterloo.ca>
2021-01-23 01:20:03 -0800

    Added 404 pages and error handling

--- src/App.js
+++ src/App.js
@@ -15,20 +15,12 @@ function App() {
         <nav className="navbar navbar-expand navbar-dark bg-dark">
           <Link to={"/"} className="navbar-brand">
             Exponent
-            </Link>
-          <div className="navbar-nav mr-auto">
-            <li className="nav-item">
-              <Link to={"/about"} className="nav-link">
-                Home
-              </Link>
-            </li>
-          </div>
+          </Link>
         </nav>
         <Switch>
           <Route path="/" exact component={Home} />
-          <Route path="/about" exact component={About} />
           <Route exact path="/quiz/:id" exact component={Quiz} />
-          <Route path="/profile" exact component={Profile} />
+          <Route component={PageNotFound} />
         </Switch>
       </BrowserRouter>
     </div>
@@ -40,16 +32,18 @@ const Home = () => {
   useEffect(() => {
     QuizService.getQuizes().then(res => {
       setQuizes(res.data);
-    });
-
-
+      //TODO CATCH ERROR
+    }).catch(err => {
+      console.log("error")
+      console.log(err)
+    })
   }, []);
   return (
     <>
       {
         quizes.length && (
           quizes.map(quiz =>
-            <Link key={quiz.id} to={{ pathname: `/quiz/${quiz.id}`, id: quiz.id }}>
+            <Link key={quiz.id} to={{ pathname: `/quiz/${quiz.id}` }}>
               <h1>{quiz.title}</h1>
             </Link>
           )
@@ -58,27 +52,45 @@ const Home = () => {
     </>
   );
 }
-const About = () => <h1>About</h1>;
+const PageNotFound = () => {
+  return (
+    <div>
+      <h1>Page Not Found</h1>
+      <p>Sorry, there is nothing to see here.</p>
+      <p><Link to="/">Back to Home</Link></p>
+    </div>
+  );
+}
 const Quiz = (props) => {
-  const [quizId, setQuizId] = useState("");
-  const [quizTitle, setQuizTitle] = useState("");
-  const [quizQuestions, setQuizQuestions] = useState([]);
+  const [quiz, setQuiz] = useState({});
   const [quizAnswers, setQuizAnswers] = useState({});
   const [quizMarked, setQuizMarked] = useState(false);
   const [quizScore, setQuizScore] = useState(0);
   const [quizQuestionsMarked, setQuestionsMarked] = useState({});
+  const [errorMessage, setErrorMessage] = useState('');
+  //use single hook for quiz 
   useEffect(() => {
-    QuizService.getQuiz(props.location.id).then(res => {
-      //console.log("Res");
-      setQuizId(res.data.id)
-      setQuizTitle(res.data.title)
-      setQuizQuestions(res.data.questions)
+
+    //TODO: ADD COMMENTS 
+    // Obtain the quiz id from url, this enables users sharing a url to a specific quiz as well
+    const { id } = props.match.params
+    QuizService.getQuiz(id).then(res => {
       console.log(res);
-    });
+      setQuiz(res.data);
+
+      //TODO:catch if error happens
+    }).catch(err => {
+      if (err.response) {
+        setErrorMessage(err.message)
+        // Catch 404 errors for quiz id not existing
+      } else {
+        setErrorMessage(err.message)
+        console.log("hiiiii")
+      }
+    })
 
   }, []);
   const handleSubmit = (event) => {
-
     event.preventDefault()
     console.log("sending data")
     /*const data = new FormData();
@@ -87,18 +99,24 @@ const Quiz = (props) => {
     const data = {
       "answers": quizAnswers
     }
-    console.log(data);
-    QuizService.submitQuiz(quizId, data).then(res => {
+    //console.log(data);
+    QuizService.submitQuiz(quiz.id, data).then(res => {
       setQuizMarked(true);
       setQuizScore(parseInt(res.data.correct));
       setQuestionsMarked(res.data.questions);
       console.log(res);
-    });
+    }).catch(err => {
+      console.log("error send")
+      console.log(err)
+      if (err.response) {
+        // Catch 400 errors for incorrectly formatting
+      }
+    })
   }
   const handleChange = (evt) => {
     evt.preventDefault();
-    console.log("evt.target.value");
-    console.log(evt.target.value);
+    //console.log("evt.target.value");
+    //console.log(evt.target.value);
     const { name, value } = evt.target;
     setQuizAnswers({
       ...quizAnswers,
@@ -108,7 +126,6 @@ const Quiz = (props) => {
   }
   const tryAgainHandler = (event) => {
     event.preventDefault();
-
     setQuizAnswers({});
     setQuizMarked(false);
     setQuizScore(0);
@@ -116,13 +133,13 @@ const Quiz = (props) => {
   }
   return (
     <>
-      {console.log(quizQuestionsMarked)}
+      {errorMessage}
       {quizScore > 0 && <h1>Your score is {quizScore}</h1>}
       <Form onSubmit={handleSubmit}>
         <Form.Group >
           {
-            quizQuestions.length && (
-              quizQuestions.map(question =>
+            Object.keys(quiz).length && (
+              quiz.questions.map(question =>
                 <div key={question.id}>
                   <p>{question.text}</p>
                   {quizMarked && <h1>{quizQuestionsMarked[question.id] && quizQuestionsMarked[question.id] ? "✔️" : "❌"}</h1>}
